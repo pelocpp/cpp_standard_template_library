64e36203d72d9fd003afe773fae8bf82ef223346
Peter Loos <peter.loos@gmx.de>
2023-07-13 17:14:14 +0200

    continued work on graphs

--- Programs/RedesignEx/Redesign01.cpp
+++ Programs/RedesignEx/Redesign01.cpp
@@ -123,6 +123,17 @@ namespace Graph_Theory_Redesign
             return true;
         }
 
+        void addNodes(const std::initializer_list<T> list) {
+
+            std::for_each(
+                list.begin(), 
+                list.end(),
+                [&](const auto& value) {
+                    addNode(value);
+                }
+            );
+        }
+
         // Returns true if the edge was successfully created, false otherwise.
         bool insertEdge(const T& from_node_value, const T& to_node_value)
         {
@@ -158,7 +169,7 @@ namespace Graph_Theory_Redesign
         }
 
         // Returns the number of nodes in the graph.
-        size_t size() const noexcept
+        size_t countNodes() const noexcept
         {
             return m_nodes.size();
         }
@@ -334,18 +345,18 @@ namespace Graph_Theory_Graphs
       //  Graph<std::string> graph{ 4 };
         Graph<std::string> graph;
 
-        graph.addNode(std::string {"A"});
-        graph.addNode(std::string {"B"});
-        graph.addNode(std::string {"C"});
-        graph.addNode(std::string {"D"});
+        //graph.addNode(std::string {"A"});
+        //graph.addNode(std::string {"B"});
+        //graph.addNode(std::string {"C"});
+        //graph.addNode(std::string {"D"});
 
-        //graph.setNodeDescriptions({
-        //    std::string{"A"},
-        //    std::string{"B"},
-        //    std::string{"C"},
-        //    std::string{"C"}
-        //    }
-        //);
+        graph.addNodes({
+            std::string{"A"},
+            std::string{"B"},
+            std::string{"C"},
+            std::string{"C"}
+            }
+        );
 
         //graph.insertEdge(0, 1);
         //graph.insertEdge(1, 2);
@@ -404,226 +415,226 @@ namespace Graph_Theory_Graphs
 // =====================================================================================
 
 
-//
-//namespace Graph_Theory_DFS
-//{
-//    using namespace Graph_Theory_Redesign;
-//
-//
-//    template <typename NodeDescription>
-//    class DFSSolver
-//    {
-//    private:
-//        Graph<NodeDescription>& m_graph;
-//        std::vector<bool>                                 m_visited;
-//        std::deque<std::vector<size_t>>                   m_paths;
-//
-//        std::vector<size_t>                               m_components;
-//        size_t                                            m_count;
-//
-//    public:
-//        DFSSolver(Graph<NodeDescription>& graph)
-//            : m_graph{ graph }, m_count{} {}
-//
-//        void computeComponents() {
-//
-//            m_visited.resize(m_graph.countNodes());
-//            std::fill(std::begin(m_visited), std::end(m_visited), false);
-//
-//            m_components.resize(m_graph.countNodes());
-//            std::fill(std::begin(m_components), std::end(m_components), 0);
-//
-//            m_count = 0;
-//
-//            for (size_t node{}; node != m_graph.countNodes(); ++node) {
-//                if (!m_visited.at(node)) {
-//                    ++m_count;
-//                    depthFirstSearch(node);
-//                }
-//            }
-//        }
-//
-//        size_t getNumberOfComponents() const { return m_count; }
-//
-//        std::vector<size_t> getComponent(size_t mark) const {
-//
-//            std::vector<size_t> result;
-//
-//            for (size_t index{}; size_t vertex : m_components) {
-//
-//                if (vertex == mark) {
-//                    result.push_back(index);
-//                }
-//
-//                ++index;
-//            }
-//
-//            return result;
-//        }
-//
-//        size_t countFoundPaths() { return m_paths.size(); }
-//
-//        // function to perform DFS traversal in a directed graph to find
-//        // the complete path between source and destination vertices
-//        bool findPathAny(size_t source, size_t target, std::vector<size_t>& path) {
-//
-//            // setup 'm_visited' vector
-//            m_visited.resize(m_graph.countNodes());
-//            std::fill(std::begin(m_visited), std::end(m_visited), false);
-//
-//            return findPathAnyHelper(source, target, path);
-//        }
-//
-//        void findPathAll(size_t source, size_t target) {
-//
-//            // setup 'm_visited' vector
-//            m_visited.resize(m_graph.countNodes());
-//            std::fill(std::begin(m_visited), std::end(m_visited), false);
-//
-//            // setup 'currentPath' vector
-//            std::vector<size_t> currentPath;
-//            currentPath.push_back(source);
-//
-//            findPathAllHelper(source, target, currentPath);
-//        }
-//
-//        void printPath(const std::vector<size_t>& path) {
-//
-//            std::for_each(
-//                std::begin(path),
-//                std::prev(std::end(path)),
-//                [](auto vertex) { std::cout << vertex << " -> "; }
-//            );
-//
-//            auto last = path.back();
-//            std::cout << last << std::endl;
-//        }
-//
-//        void printPaths() {
-//            for (const auto& path : m_paths) {
-//                printPath(path);
-//            }
-//        }
-//
-//    private:
-//        void depthFirstSearch(size_t node) {
-//
-//            m_visited.at(node) = true;          // mark current node as discovered
-//            m_components.at(node) = m_count;    // set mark
-//
-//            // do for all adjacent vertices of the current vertex
-//            std::vector<size_t> neighbours = m_graph.getNeighbouringNodes(node);
-//            for (size_t next : neighbours) {
-//
-//                // next is not discovered
-//                if (!m_visited.at(next)) {
-//
-//                    depthFirstSearch(next);
-//                }
-//            }
-//        }
-//
-//        bool findPathAnyHelper(size_t source, size_t target, std::vector<size_t>& path) {
-//
-//            // mark current node as discovered
-//            m_visited.at(source) = true;
-//
-//            // include current node in the path
-//            path.push_back(source);
-//
-//            // if destination vertex is found
-//            if (source == target) {
-//                return true;
-//            }
-//
-//            // do for all adjacent vertices of the dequeued vertex
-//            std::vector<size_t> neighbours = m_graph.getNeighbouringNodes(source);
-//
-//            for (size_t next : neighbours) {
-//
-//                // next is not discovered
-//                if (!m_visited.at(next)) {
-//
-//                    // return true if destination is found
-//                    if (findPathAnyHelper(next, target, path))
-//                        return true;
-//                }
-//            }
-//
-//            // backtrack: remove current node from the path
-//            path.pop_back();
-//
-//            // return false if destination vertex is not reachable from source
-//            return false;
-//
-//        }
-//
-//        void findPathAllHelper(size_t source, size_t target, std::vector<size_t>& path) {
-//
-//            // mark current node as discovered
-//            m_visited.at(source) = true;
-//
-//            // if destination vertex is found
-//            if (source == target) {
-//                m_paths.push_back(path); // store found solution
-//                m_visited.at(source) = false;  // unmark current node as discovered
-//            }
-//            else {
-//                // do for every edge
-//                std::vector<size_t> neighbours = m_graph.getNeighbouringNodes(source);
-//
-//                for (size_t next : neighbours) {
-//
-//                    // next is not discovered
-//                    if (!m_visited.at(next)) {
-//                        path.push_back(next);  // include current node in the path
-//                        findPathAllHelper(next, target, path);
-//                        path.pop_back();  // remove current node from the path (backtrack)
-//                    }
-//                }
-//
-//                // unmark current node as discovered
-//                m_visited.at(source) = false;
-//            }
-//        }
-//    };
-//
-//    // DFS - all paths - UNDIRECTED Graph - NOT WEIGHTED
-//    void test_dfs_01()
-//    {
-//        std::cout << "Redesign Graph Theory - DFS" << std::endl;
-//
-//        Graph<int> graph{ 8 };
-//
-//        graph.addEdge(0, 3);
-//        graph.addEdge(1, 0);
-//        graph.addEdge(1, 2);
-//        graph.addEdge(1, 4);
-//        graph.addEdge(2, 7);
-//        graph.addEdge(3, 4);
-//        graph.addEdge(3, 5);
-//        graph.addEdge(4, 3);
-//        graph.addEdge(4, 6);
-//        graph.addEdge(5, 6);
-//        graph.addEdge(6, 7);
-//
-//        DFSSolver dfs{ graph };
-//
-//        constexpr size_t Source{ 1 };
-//        constexpr size_t Target{ 6 };
-//
-//        dfs.findPathAll(Source, Target);
-//
-//        if (size_t count; (count = dfs.countFoundPaths()) != 0)
-//        {
-//            std::cout << "Found " << count << " solutions:" << std::endl;
-//            dfs.printPaths();
-//        }
-//        else {
-//            std::cout << "No path exists between vertices " << Source << " and " << Target;
-//        }
-//    }
-//}
+namespace Graph_Theory_DFS
+{
+    using namespace Graph_Theory_Redesign;
+
+    template <typename T>
+    class DFSSolver
+    {
+    private:
+        Graph<T>&                          m_graph;
+        std::vector<bool>                  m_visited;
+        std::deque<std::vector<size_t>>    m_paths;
+
+        std::vector<size_t>                m_components;
+        size_t                             m_count;
+
+    public:
+        DFSSolver(Graph<T>& graph) : m_graph{ graph }, m_count{} {}
+
+        void computeComponents() {
+
+            m_visited.resize(m_graph.countNodes());
+            std::fill(std::begin(m_visited), std::end(m_visited), false);
+
+            m_components.resize(m_graph.countNodes());
+            std::fill(std::begin(m_components), std::end(m_components), 0);
+
+            m_count = 0;
+
+            for (size_t node{}; node != m_graph.countNodes(); ++node) {
+                if (!m_visited.at(node)) {
+                    ++m_count;
+                    depthFirstSearch(node);
+                }
+            }
+        }
+
+        size_t getNumberOfComponents() const { return m_count; }
+
+        std::vector<size_t> getComponent(size_t mark) const {
+
+            std::vector<size_t> result;
+
+            for (size_t index{}; size_t vertex : m_components) {
+
+                if (vertex == mark) {
+                    result.push_back(index);
+                }
+
+                ++index;
+            }
+
+            return result;
+        }
+
+        size_t countFoundPaths() { return m_paths.size(); }
+
+        // function to perform DFS traversal in a directed graph to find
+        // the complete path between source and destination vertices
+        bool findPathAny(size_t source, size_t target, std::vector<size_t>& path) {
+
+            // setup 'm_visited' vector
+            m_visited.resize(m_graph.countNodes());
+            std::fill(std::begin(m_visited), std::end(m_visited), false);
+
+            return findPathAnyHelper(source, target, path);
+        }
+
+        void findPathAll(size_t source, size_t target) {
+
+            // setup 'm_visited' vector
+            m_visited.resize(m_graph.countNodes());
+            std::fill(std::begin(m_visited), std::end(m_visited), false);
+
+            // setup 'currentPath' vector
+            std::vector<size_t> currentPath;
+            currentPath.push_back(source);
+
+            findPathAllHelper(source, target, currentPath);
+        }
+
+        void printPath(const std::vector<size_t>& path) {
+
+            std::for_each(
+                std::begin(path),
+                std::prev(std::end(path)),
+                [](auto vertex) { std::cout << vertex << " -> "; }
+            );
+
+            auto last = path.back();
+            std::cout << last << std::endl;
+        }
+
+        void printPaths() {
+            for (const auto& path : m_paths) {
+                printPath(path);
+            }
+        }
+
+    private:
+        void depthFirstSearch(size_t node) {
+
+            m_visited.at(node) = true;          // mark current node as discovered
+            m_components.at(node) = m_count;    // set mark
+
+            // do for all adjacent vertices of the current vertex
+            std::vector<size_t> neighbours = m_graph.getNeighbouringNodes(node);
+            for (size_t next : neighbours) {
+
+                // next is not discovered
+                if (!m_visited.at(next)) {
+
+                    depthFirstSearch(next);
+                }
+            }
+        }
+
+        bool findPathAnyHelper(size_t source, size_t target, std::vector<size_t>& path) {
+
+            // mark current node as discovered
+            m_visited.at(source) = true;
+
+            // include current node in the path
+            path.push_back(source);
+
+            // if destination vertex is found
+            if (source == target) {
+                return true;
+            }
+
+            // do for all adjacent vertices of the dequeued vertex
+            std::vector<size_t> neighbours = m_graph.getNeighbouringNodes(source);
+
+            for (size_t next : neighbours) {
+
+                // next is not discovered
+                if (!m_visited.at(next)) {
+
+                    // return true if destination is found
+                    if (findPathAnyHelper(next, target, path))
+                        return true;
+                }
+            }
+
+            // backtrack: remove current node from the path
+            path.pop_back();
+
+            // return false if destination vertex is not reachable from source
+            return false;
+
+        }
+
+        void findPathAllHelper(size_t source, size_t target, std::vector<size_t>& path) {
+
+            // mark current node as discovered
+            m_visited.at(source) = true;
+
+            // if destination vertex is found
+            if (source == target) {
+                m_paths.push_back(path); // store found solution
+                m_visited.at(source) = false;  // unmark current node as discovered
+            }
+            else {
+                // do for every edge
+                std::vector<size_t> neighbours = m_graph.getNeighbouringNodes(source);
+
+                for (size_t next : neighbours) {
+
+                    // next is not discovered
+                    if (!m_visited.at(next)) {
+                        path.push_back(next);  // include current node in the path
+                        findPathAllHelper(next, target, path);
+                        path.pop_back();  // remove current node from the path (backtrack)
+                    }
+                }
+
+                // unmark current node as discovered
+                m_visited.at(source) = false;
+            }
+        }
+    };
+
+    // DFS - all paths - UNDIRECTED Graph - NOT WEIGHTED
+    void test_dfs_01()
+    {
+        std::cout << "Redesign Graph Theory - DFS" << std::endl;
+
+        // Graph<int> graph{ 8 };
+        Graph<int> graph;
+
+        graph.addNodes({0, 1, 2, 3, 4, 5, 6, 7});
+
+        graph.insertEdge(0, 3);
+        graph.insertEdge(1, 0);
+        graph.insertEdge(1, 2);
+        graph.insertEdge(1, 4);
+        graph.insertEdge(2, 7);
+        graph.insertEdge(3, 4);
+        graph.insertEdge(3, 5);
+        graph.insertEdge(4, 3);
+        graph.insertEdge(4, 6);
+        graph.insertEdge(5, 6);
+        graph.insertEdge(6, 7);
+
+        DFSSolver dfs{ graph };
+
+        constexpr size_t Source{ 1 };
+        constexpr size_t Target{ 6 };
+
+        dfs.findPathAll(Source, Target);
+
+        if (size_t count; (count = dfs.countFoundPaths()) != 0)
+        {
+            std::cout << "Found " << count << " solutions:" << std::endl;
+            dfs.printPaths();
+        }
+        else {
+            std::cout << "No path exists between vertices " << Source << " and " << Target;
+        }
+    }
+}
 
 // =====================================================================================
 // End-of-File
