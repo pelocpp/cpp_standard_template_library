45302c70b1caeba6f997d100a5d32770de89a891
Peter Loos <peter.loos@gmx.de>
2023-08-15 18:02:30 +0200

    first running version of iterator implemented

--- Programs/RedesignEx/Test_ConstGraphIterator.cpp
+++ Programs/RedesignEx/Test_ConstGraphIterator.cpp
@@ -41,18 +41,46 @@ void test_graphs_iterators_02()
 
     ConstGraphIterator<size_t> iter = graph.begin();
 
-    auto elem = *iter;
+    auto& elem = *iter;
 
     ++iter;
 
-    elem = *iter;
+    auto& elem2 = *iter;
+}
+
+void test_graphs_iterators_03()
+{
+    // b) Directed, Weighted
+
+    Graph<std::string, size_t> graph{ Direction::Directed, Weight::Weighted };
+
+    graph.addNodes({ "A", "B", "C", "D" });
+
+    graph.addEdge(std::string{ "A" }, std::string{ "B" }, 10);
+    graph.addEdge(std::string{ "B" }, std::string{ "C" }, 20);
+    graph.addEdge(std::string{ "C" }, std::string{ "C" }, 10);
+    graph.addEdge(std::string{ "C" }, std::string{ "A" }, 50);
+
+    std::cout << graph.toString() << std::endl;
+
+    std::cout << graph.toStringRaw() << std::endl;
+
+    ConstGraphIterator<std::string, size_t> iter = graph.begin();
+
+    auto& elem = *iter;
+
+    ++iter;
+
+    auto& elem2 = *iter;
 }
 
 // =======================================================================
 
 void test_graph_iterators()
 {
-    test_graphs_iterators_02();
+    //test_graphs_iterators_01();
+    //test_graphs_iterators_02();
+    test_graphs_iterators_03();
 }
 
 // =====================================================================================
